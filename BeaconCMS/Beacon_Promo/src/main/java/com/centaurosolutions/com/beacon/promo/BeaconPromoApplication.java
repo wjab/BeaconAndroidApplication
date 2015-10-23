85fbb649e572b92fdd1c6c4c80327cf2d1426de6
eduartnav03 <eduardo.trejos@centaurosolutions.com>
2015-10-23 10:00:32 -0600

    Revert "Prueba GitHub"
    This reverts commit 80801b63f8fddadc3885a5cf85e0ef6896a94e8c.

--- BeaconCMS/Beacon_Promo/src/main/java/com/centaurosolutions/com/beacon/promo/BeaconPromoApplication.java
+++ BeaconCMS/Beacon_Promo/src/main/java/com/centaurosolutions/com/beacon/promo/BeaconPromoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeaconPromoApplication {
 
-	
     public static void main(String[] args) {
         SpringApplication.run(BeaconPromoApplication.class, args);
     }
