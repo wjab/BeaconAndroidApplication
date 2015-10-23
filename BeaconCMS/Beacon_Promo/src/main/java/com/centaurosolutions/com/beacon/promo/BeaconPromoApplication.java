0fc7438d15244b8afb6af09da43497e9fd37d9e9
eduartnav03 <eduardo.trejos@centaurosolutions.com>
2015-10-23 09:59:56 -0600

    Prueba GitHub

--- BeaconCMS/Beacon_Promo/src/main/java/com/centaurosolutions/com/beacon/promo/BeaconPromoApplication.java
+++ BeaconCMS/Beacon_Promo/src/main/java/com/centaurosolutions/com/beacon/promo/BeaconPromoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeaconPromoApplication {
 
+	
     public static void main(String[] args) {
         SpringApplication.run(BeaconPromoApplication.class, args);
     }
