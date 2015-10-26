282ff4651206a70b4953cd6bcf986eb91b650f29
eduartnav03 <eduardo.trejos@centaurosolutions.com>
2015-10-26 15:45:03 -0600

    Finalización de API REST para Devices (reverted from commit a89d4ed72d7d651844b66539a0adaf15a541d6b6)

--- BeaconCMS/Beacon_Device/src/main/java/com/centaurosolutions/com/beacon/device/repository/DeviceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.centaurosolutions.com.beacon.device.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.centaurosolutions.com.beacon.device.model.Device;
-
-public  interface DeviceRepository extends MongoRepository<Device, String> {
-
-}
---
+++ BeaconCMS/Beacon_Device/src/main/java/com/centaurosolutions/com/beacon/device_model/DeviceModel.java
@@ -0,0 +1,5 @@
+package com.centaurosolutions.com.beacon.device_model;
+
+public class DeviceModel {
+
+}
