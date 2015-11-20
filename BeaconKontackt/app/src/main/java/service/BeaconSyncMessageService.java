021a43566796fe59f2d06958950f829feb94966f
eduartnav03 <eduardo.trejos@centaurosolutions.com>
2015-11-20 08:49:53 -0600

    Manejo de Tasks para el servicio de sincronización de expiración, ahora el servicio para actualizar las expiraciones se corre cada 5 segundos en background

--- BeaconKontackt/app/src/main/java/service/BeaconSyncMessageService.java
+++ BeaconKontackt/app/src/main/java/service/BeaconSyncMessageService.java
@@ -5,29 +5,17 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
+import java.util.Timer;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import task.BeaconMessageSyncTask;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public class BeaconSyncMessageService extends Service  {
 
-import controllers.ServiceController;
-import database.DatabaseManager;
-import model.BeaconCache;
 
-public class BeaconSyncMessageService extends Service implements Response.Listener<JSONObject>, Response.ErrorListener {
 
-    ServiceController serviceController;
-    Response.Listener<JSONObject> response;
-    Response.ErrorListener responseError;
-    BeaconCache myBeaconCache;
-    List<BeaconCache> beaconList;
+    public static final long SYNC_INTERVAL = 5 * 1000;
+
+    private Timer mTimer = null;
 
     public BeaconSyncMessageService() {
 
@@ -37,114 +25,24 @@ public class BeaconSyncMessageService extends Service implements Response.Listen
     public void onCreate() {
         super.onCreate();
         Log.i("BSMS", "Service onCreate");
-    }
 
-    @Override
-    public void onDestroy() {
-        Log.i("BSMS", "Service onStop");
-        super.onDestroy();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("BSMS", "Service Start");
-
-        beaconList = DatabaseManager.getInstance().getAllBeaconCache();
-        if(beaconList !=null){
-            Log.i("BSMS","DB not null");
-            sendPromoRequest();
+        // cancel if already existed
+        if (mTimer != null) {
+            mTimer.cancel();
+        } else {
+            // recreate new
+            mTimer = new Timer();
         }
-
-
-
-
-        return Service.START_STICKY;
+        // schedule task
+        mTimer.scheduleAtFixedRate(new BeaconMessageSyncTask(), 0, SYNC_INTERVAL);
+        Log.i("BSMS", "Task Started");
     }
+
+
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-
-    public void sendPromoRequest(){
-        Log.i("BSMS","Request sending");
-        serviceController =  new ServiceController();
-        responseError = this;
-        response = this;
-
-
-        Map<String,String> nullMap =  new HashMap<String, String>();
-
-        Map<String, String> map = new HashMap<String, String>();
-        map.put("Content-Type", "application/json");
-        String url = "http://beacon_promo_development.cfapps.io/promo/exp";
-        serviceController.jsonObjectRequest(url, Request.Method.GET, null, map, response,responseError);
-
-
-    }
-
-    public void addBeaconDB(BeaconCache beaconObject){
-
-        beaconList = DatabaseManager.getInstance().getAllBeaconCache();
-        if(beaconList!=null){
-            for (BeaconCache myCache:beaconList) {
-                if(!beaconObject.getUniqueID().equals(myCache.getUniqueID()) && !beaconObject.getProximity().equals(myCache.getProximity())){
-                    DatabaseManager.getInstance().addBeaconCache(beaconObject);
-                }
-                else {
-                    DatabaseManager.getInstance().updateBeaconCache(beaconObject);
-                }
-            }
-        }
-        else{
-            DatabaseManager.getInstance().addBeaconCache(beaconObject);
-        }
-    }
-    @Override
-    public void onResponse(JSONObject response) {
-        Log.i("BSMS", "Request received");
-
-        beaconList = DatabaseManager.getInstance().getAllBeaconCache();
-        Map<String,Double> values = new HashMap<String,Double>();
-
-        try {
-            JSONArray promoArray = response.getJSONArray("PromosExp");
-            for(int i =0; i < promoArray.length();i++){
-                Log.i("BSMS", promoArray.getJSONObject(i).getString("promoId"));
-
-                for(int j = 0; j < beaconList.size(); j++){
-                    if(beaconList.get(j).getPromoId().equals(promoArray.getJSONObject(i).getString("promoId"))){
-                        Log.i("BSMS", beaconList.get(j).getUniqueID() + " " + promoArray.getJSONObject(i).getString("promoId"));
-                        BeaconCache myBeacon = new BeaconCache();
-                        myBeacon.setPromoId(beaconList.get(j).getPromoId());
-                        myBeacon.setUniqueID(beaconList.get(j).getUniqueID());
-                        myBeacon.setMessage(beaconList.get(j).getMessage());
-                        myBeacon.setProximity(beaconList.get(j).getProximity());
-                        myBeacon.setExpiration(promoArray.getJSONObject(i).getDouble("expiration"));
-                        DatabaseManager.getInstance().updateBeaconCache(myBeacon);
-                    }
-                }
-
-
-                values.put(promoArray.getJSONObject(i).getString("promoId"), promoArray.getJSONObject(i).getDouble("expiration"));
-            }
-
-
-
-
-
-
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    @Override
-    public void onErrorResponse(VolleyError error) {
-        Log.d("Login Error", error.toString());
-    }
 }
---
+++ BeaconKontackt/app/src/main/java/task/BeaconMessageSyncTask.java
@@ -0,0 +1,119 @@
+package task;
+
+import android.os.Handler;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import controllers.ServiceController;
+import database.DatabaseManager;
+import model.BeaconCache;
+
+/**
+ * Created by Eduardo on 20/11/2015.
+ */
+public class BeaconMessageSyncTask extends TimerTask implements Response.Listener<JSONObject>, Response.ErrorListener {
+
+    ServiceController serviceController;
+    Response.Listener<JSONObject> response;
+    Response.ErrorListener responseError;
+    BeaconCache myBeaconCache;
+    List<BeaconCache> beaconList;
+
+    public static final long SYNC_INTERVAL = 5 * 1000;
+
+
+    private Handler mHandler = new Handler();
+
+    private Timer mTimer = null;
+
+
+    public void sendPromoRequest() {
+        Log.i("BSMS", "Request sending");
+        serviceController = new ServiceController();
+        responseError = this;
+        response = this;
+
+
+        Map<String, String> nullMap = new HashMap<String, String>();
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Content-Type", "application/json");
+        String url = "http://beacon_promo_development.cfapps.io/promo/exp";
+        serviceController.jsonObjectRequest(url, Request.Method.GET, null, map, response, responseError);
+
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Log.i("BSMS", "Request received");
+
+        beaconList = DatabaseManager.getInstance().getAllBeaconCache();
+        Map<String, Double> values = new HashMap<String, Double>();
+
+        try {
+            JSONArray promoArray = response.getJSONArray("PromosExp");
+            for (int i = 0; i < promoArray.length(); i++) {
+                Log.i("BSMS", promoArray.getJSONObject(i).getString("promoId"));
+
+                for (int j = 0; j < beaconList.size(); j++) {
+                    if (beaconList.get(j).getPromoId().equals(promoArray.getJSONObject(i).getString("promoId"))) {
+                        Log.i("BSMS", beaconList.get(j).getUniqueID() + " " + promoArray.getJSONObject(i).getString("promoId"));
+                        BeaconCache myBeacon = new BeaconCache();
+                        myBeacon.setPromoId(beaconList.get(j).getPromoId());
+                        myBeacon.setUniqueID(beaconList.get(j).getUniqueID());
+                        myBeacon.setMessage(beaconList.get(j).getMessage());
+                        myBeacon.setProximity(beaconList.get(j).getProximity());
+                        myBeacon.setExpiration(promoArray.getJSONObject(i).getDouble("expiration"));
+                        DatabaseManager.getInstance().updateBeaconCache(myBeacon);
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Log.d("Login Error", error.toString());
+    }
+
+    @Override
+    public void run() {
+        // run on another thread
+        mHandler.post(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Log.i("BSMS", "Running Task");
+                beaconList = DatabaseManager.getInstance().getAllBeaconCache();
+                if (beaconList != null) {
+                    Log.i("BSMS", "DB not null");
+                    sendPromoRequest();
+                }
+
+            }
+
+        });
+    }
+
+
+
+}
