e50b704d79c7fe9dc35670118b762a3f56262c06
walter <walter@centaurosolutions.com>
2015-12-17 18:21:21 -0600

    Se sube el proyecto de utilizades que sirve como wrapper con los servicios rest

---
+++ BeaconCMS/Beacon_Utils/src/main/java/com/beaconutils/BeaconUtilsApplication.java
@@ -0,0 +1,12 @@
+package com.beaconutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaconUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeaconUtilsApplication.class, args);
+    }
+}
