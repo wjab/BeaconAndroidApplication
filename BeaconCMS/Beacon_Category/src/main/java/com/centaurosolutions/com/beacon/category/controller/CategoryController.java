556c844e91edf2627f3ffb49da7e9679998f7f2b
jarivar <jairo@centaurosolutions.com>
2016-05-17 08:42:49 -0600

    refactoring y validaciones del servicio de categorias

--- BeaconCMS/Beacon_Category/src/main/java/com/centaurosolutions/com/beacon/category/controller/CategoryController.java
+++ BeaconCMS/Beacon_Category/src/main/java/com/centaurosolutions/com/beacon/category/controller/CategoryController.java
@@ -45,29 +45,86 @@ public class CategoryController {
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public Map<String, Object> createCategory(@RequestBody Map<String, Object> categoryMap){
-		
-		 Category category=new Category();
-		 try{
-		 category = new Category(Integer.parseInt(categoryMap.get("startRange").toString()),Integer.parseInt(categoryMap.get("endRange").toString()), Integer.parseInt(categoryMap.get("giftPoints").toString()) ,DateFormatter(categoryMap.get("creationDate").toString()),DateFormatter(categoryMap.get("modifieldDate").toString()),categoryMap.get("categoryName").toString(),(Boolean)categoryMap.get("enable"));
-		}catch(Exception ex){}
 		Map<String, Object> response = new LinkedHashMap<String, Object>();
-	    response.put("message", "Categoría creada correctamente");
-	    response.put("category", category);		
-	    categoryRepository.save(category);
-		return response;
+		try{
+		   Category category=new Category();
+		   Category category_Find=categoryRepository.findByCategoryName(categoryMap.get("categoryName").toString());
+		   if (category_Find==null)
+		   {
+		       category = new Category(Integer.parseInt(categoryMap.get("startRange").toString()),
+				 								  Integer.parseInt(categoryMap.get("endRange").toString()),
+				 								  Integer.parseInt(categoryMap.get("giftPoints").toString()),
+				 								  DateFormatter(categoryMap.get("creationDate").toString()),
+				 								  DateFormatter(categoryMap.get("modifieldDate").toString()),
+				 								  categoryMap.get("categoryName").toString(),
+				 								  (Boolean)categoryMap.get("enable"));
+		       categoryRepository.save(category);
+		       response.put("message", "Categoría creada correctamente");
+		       response.put("category", category);
+		       response.put("status", "200");
+		      
+		  }
+		  else
+		  {
+		       response.put("message", "Categoría ya existe");
+		       response.put("category", null);
+		       response.put("status", "400"); 
+		  }
+		}
+		catch(Exception ex)
+		{
+		    response.put("message", ex.getMessage());
+		    response.put("category", null);
+		    response.put("status", "400"); 
+		}		
+		return response;  
 	}
 	
 	 @RequestMapping(method = RequestMethod.GET, value="/{categoryId}")
-	  public Category getCategoryDetails(@PathVariable("categoryId") String categoryId){
-	    return categoryRepository.findOne(categoryId);
+	  public Map<String, Object> getCategoryDetails(@PathVariable("categoryId") String categoryId){
+		 Map<String, Object> response = new LinkedHashMap<String, Object>();
+		 try
+		 {
+			  Category category_Find;
+			  category_Find= categoryRepository.findOne(categoryId);
+			  if (category_Find!=null) 
+			  {
+				  response.put("message", "Categoria encontrada");
+				  response.put("category", category_Find);
+				  response.put("status", "200"); 
+			  }
+			  else
+			  {
+				  response.put("message", "Categoria no encontrada");
+				  response.put("category", null);
+				  response.put("status", "404"); 
+			  }			  
+		 } 
+		 catch(Exception ex)
+		 {
+		     response.put("message", ex.getMessage());
+			 response.put("category", null);
+			 response.put("status", "400"); 
+		 }
+		 return response;
 	  }
 	 
 	  @RequestMapping(method = RequestMethod.GET)
 	  public Map<String, Object> getAllCategoriesDetails(){
-		  List<Category> categories = categoryRepository.findAll();
 		  Map<String, Object> response = new LinkedHashMap<String, Object>();
-		  response.put("Total de categorías", categories.size());
-		  response.put("Categories", categories);
+		  try
+		  {
+		      List<Category> categories = categoryRepository.findAll();		 
+		      response.put("Total de categorías", categories.size());
+		      response.put("Categories", categories);
+		      response.put("status", "200"); 
+		  }
+		  catch(Exception ex)
+		  {
+			  response.put("message", ex.getMessage());
+			  response.put("category", null);
+			  response.put("status", "400"); 
+		  }
 		  return response;
 	  }
 	  
@@ -75,23 +132,40 @@ public class CategoryController {
 	  @RequestMapping(method = RequestMethod.PUT, value="/{CategoryId}")
 	  public Map<String, Object> editCategory(@PathVariable("CategoryId") String CategoryId,
 	      @RequestBody Map<String, Object> categoryMap){
-		  Category category=new Category();
-			 try{
+		  Map<String, Object> response = new LinkedHashMap<String, Object>();
+		  try 
+		  {
+	         Category category=new Category();
 			 category = new Category(Integer.parseInt(categoryMap.get("startRange").toString()),Integer.parseInt(categoryMap.get("endRange").toString()), Integer.parseInt(categoryMap.get("giftPoints").toString()) ,DateFormatter(categoryMap.get("creationDate").toString()),DateFormatter(categoryMap.get("modifieldDate").toString()),categoryMap.get("categoryName").toString(),(Boolean)categoryMap.get("enable"));
-			}catch(Exception ex){} 
-			 category.setId(CategoryId);
-	    Map<String, Object> response = new LinkedHashMap<String, Object>();
-	    response.put("message", "Categoría actualizada correctamente");
-	    response.put("Category", categoryRepository.save(category));
-	    return response;
+			 category.setId(CategoryId);	  
+			 response.put("message", "Categoría actualizada correctamente");
+			 response.put("Category", categoryRepository.save(category));
+			 response.put("status", "200"); 
+		  }
+		  catch(Exception ex)
+		  {
+			  response.put("message", ex.getMessage());
+			  response.put("category", null);
+			  response.put("status", "400"); 
+		  }
+	      return response;
 	  }
 	  
 	  
 	  @RequestMapping(method = RequestMethod.DELETE, value="/{CategoryId}")
 	  public Map<String, String> deleteCategory(@PathVariable("CategoryId") String CategoryId){
-	    categoryRepository.delete(CategoryId);
-	    Map<String, String> response = new HashMap<String, String>();
-	    response.put("message", "Categoría eliminada correctamente");
-	    return response;
-	  }
+	      Map<String, String> response = new HashMap<String, String>();
+		  try 
+		  {
+		      categoryRepository.delete(CategoryId);
+		      response.put("message", "Categoría eliminada correctamente");
+	      }
+		  catch(Exception ex)
+		  {
+			  response.put("message", ex.getMessage());
+			  response.put("category", null);
+			  response.put("status", "400"); 
+		  }
+		  return response;
+	 }
 }
--- BeaconCMS/Beacon_Category/src/main/java/com/centaurosolutions/com/beacon/category/repository/CategoryRepository.java
+++ BeaconCMS/Beacon_Category/src/main/java/com/centaurosolutions/com/beacon/category/repository/CategoryRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.centaurosolutions.com.beacon.category.model.Category;
 
 public interface CategoryRepository extends MongoRepository<Category, String> {
-
+	Category findByCategoryName(String user);
 }
