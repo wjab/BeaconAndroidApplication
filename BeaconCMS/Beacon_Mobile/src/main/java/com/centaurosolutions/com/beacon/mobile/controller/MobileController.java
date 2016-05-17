599e7440626081590d26dca753855ebcd42faaa2
jarivar <jairo@centaurosolutions.com>
2016-05-17 11:54:38 -0600

    refactorizacion y validaciones al servicio de mobile

--- BeaconCMS/Beacon_Mobile/src/main/java/com/centaurosolutions/com/beacon/mobile/controller/MobileController.java
+++ BeaconCMS/Beacon_Mobile/src/main/java/com/centaurosolutions/com/beacon/mobile/controller/MobileController.java
@@ -40,64 +40,147 @@ public class MobileController {
 
 	@RequestMapping(method = RequestMethod.POST)
 	public Map<String, Object> createMobile(@RequestBody Map<String, Object> mobileMap){
-		
-			
-		Mobile mobileModel = new Mobile(mobileMap.get("deviceName").toString(),mobileMap.get("deviceModel").toString(),mobileMap.get("osName").toString(), mobileMap.get("osVersion").toString(),mobileMap.get("userId").toString(), DateFormatter(new Date().toString()));       
 		Map<String, Object> response = new LinkedHashMap<String, Object>();
-	    response.put("message", "Dispositivo Móvil creado correctamente");
-	    response.put("mobile", mobileModel); 					
-						
-		mobileRepository.save(mobileModel);	
-	
-
+		try
+		{
+			Mobile mobileModel = new Mobile(mobileMap.get("deviceName").toString(),
+										mobileMap.get("deviceModel").toString(),
+										mobileMap.get("osName").toString(),
+										mobileMap.get("osVersion").toString(),
+										mobileMap.get("userId").toString(),
+										DateFormatter(new Date().toString()));       
+		
+			response.put("message", "Dispositivo Móvil creado correctamente");
+			response.put("mobile", mobileModel);
+			response.put("status","200");						
+			mobileRepository.save(mobileModel);	
+		}
+		catch(Exception ex){
+			response.put("message", ex.getMessage());
+			response.put("mobile", null);
+			response.put("status","400");	
+		}
 		return response;
 	}
 	
 	  @RequestMapping(method = RequestMethod.GET, value="/{MobileId}")
-	  public Mobile getMobileDetails(@PathVariable("MobileId") String MobileId){
-	    return mobileRepository.findOne(MobileId);
+	  public Map<String, Object>  getMobileDetails(@PathVariable("MobileId") String MobileId){
+		  Map<String, Object> response = new LinkedHashMap<String, Object>();
+		  try
+		  {
+			  Mobile mobile= mobileRepository.findOne(MobileId);
+			  if(mobile!=null)
+			  {
+				  response.put("message",  "Dispositivo Móvil creado correctamente");
+				  response.put("mobile", mobile);
+				  response.put("status","200");	
+			  }
+			  else
+			  {
+				  response.put("message", "Dispositivo Móvil no encontrado");
+				  response.put("mobile", null);
+				  response.put("status","401");	 
+			  }		  
+		  }
+		  catch(Exception ex)
+		  {
+			  response.put("message", ex.getMessage());
+			  response.put("mobile", null);
+			  response.put("status","400");	
+		  }
+		  return response;
 	  }
 	  
 	  @RequestMapping(method = RequestMethod.GET, value="/user/{UserId}")
 	  public Map<String, Object>  getMobileByUser(@PathVariable("UserId") String UserId){
-		  List<Mobile> mobileModelList = mobileRepository.findByUserId(UserId);
 		  Map<String, Object> response = new LinkedHashMap<String, Object>();
-		  response.put("Total de dispositivos móviles", mobileModelList.size());
-		  response.put("MobileList", mobileModelList);
-		  
+		  try
+		  {
+			  List<Mobile> mobileModelList = mobileRepository.findByUserId(UserId);
+			  response.put("Total de dispositivos móviles", mobileModelList.size());
+			  response.put("MobileList", mobileModelList);
+			  response.put("status","200");	
+		  }
+		  catch(Exception ex)
+		  {
+			  response.put("message", ex.getMessage());
+			  response.put("mobile", null);
+			  response.put("status","400");	
+		  }
 		  return response;
 	  }
 	  
 	  @RequestMapping(method = RequestMethod.GET)
 	  public Map<String, Object> getAllPromoDetails(){
-		  List<Mobile> mobileModelList = mobileRepository.findAll();
 		  Map<String, Object> response = new LinkedHashMap<String, Object>();
-		  response.put("Total de dispositivos móviles", mobileModelList.size());
-		  response.put("MobileList", mobileModelList);
+		  try
+		  {
+			  List<Mobile> mobileModelList = mobileRepository.findAll();
+			  response.put("Total de dispositivos móviles", mobileModelList.size());
+			  response.put("MobileList", mobileModelList);
+			  response.put("status","200");	
+		  }
+		  catch(Exception ex)
+		  {
+			  response.put("message", ex.getMessage());
+			  response.put("mobile", null);
+			  response.put("status","400");	
+		  }
 		  return response;
-	  }
+	}
 	  
 	   
 	  @RequestMapping(method = RequestMethod.PUT, value="/{MobileId}")
 	  public Map<String, Object> editMobile(@PathVariable("MobileId") String MobileId,
 	      @RequestBody Map<String, Object> mobileMap){
-		  
-
-		Mobile mobileModel = new Mobile(mobileMap.get("deviceName").toString(),mobileMap.get("deviceModel").toString(),mobileMap.get("osName").toString(), mobileMap.get("osVersion").toString(),mobileMap.get("userId").toString(), DateFormatter(new Date().toString()));
-		mobileModel.setId(MobileId);
-	    Map<String, Object> response = new LinkedHashMap<String, Object>();
-	    response.put("message", "Dispositivo Móvil actualizado correctamente");
-	    response.put("mobile", mobileRepository.save(mobileModel));
-	    return response;
+		  Map<String, Object> response = new LinkedHashMap<String, Object>();
+		  try
+		  {
+			  Mobile mobile_finded=mobileRepository.findOne(MobileId);
+			  if(mobile_finded!=null)
+			  {
+				  Mobile mobileModel = new Mobile(mobileMap.get("deviceName").toString(),
+						  						  mobileMap.get("deviceModel").toString(),
+						  						  mobileMap.get("osName").toString(),
+						  						  mobileMap.get("osVersion").toString(),
+						  						  mobileMap.get("userId").toString(),
+						  						  DateFormatter(new Date().toString()));
+				  mobileModel.setId(MobileId);
+				  response.put("message", "Dispositivo Móvil actualizado correctamente");
+				  response.put("mobile", mobileRepository.save(mobileModel));
+				  response.put("status","200");	
+			  }
+			  else
+			  {
+				  response.put("message", "Dispositivo Móvil no existe");
+				  response.put("mobile", null);
+				  response.put("status","401");	
+			  }
+		  }
+		  catch(Exception ex)
+		  {
+			  response.put("message", ex.getMessage());
+			  response.put("mobile", null);
+			  response.put("status","400");	
+		  }
+		  return response;
 	  }
 	
 	  @RequestMapping(method = RequestMethod.DELETE, value="/{MobileId}")
 	  public Map<String, String> deleteMobile(@PathVariable("MobileId") String MobileId){
-	    mobileRepository.delete(MobileId);
-	    Map<String, String> response = new HashMap<String, String>();
-	    response.put("message", "Dispositivo Móvil eliminado correctamente");
-
-	    return response;
+		  Map<String, String> response = new HashMap<String, String>();
+		  try{
+			  mobileRepository.delete(MobileId);
+			  response.put("message", "Dispositivo Móvil eliminado correctamente");
+			  response.put("status","200");
+		  }
+		  catch(Exception ex)
+		  {
+			  response.put("message", ex.getMessage());
+			  response.put("mobile", null);
+			  response.put("Status","400");	
+		  }
+		  return response;
 	  }
 
 		private Date DateFormatter(String pDate){
