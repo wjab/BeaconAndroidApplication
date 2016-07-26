1ddb14f8f3a150e5e92a9d8e48cae8b88badee09
eduartnav03 <eduardo.trejos@centaurosolutions.com>
2016-07-26 09:17:32 -0600

    Nuevo proyecto para puntos

---
+++ BeaconCMS/Beacon_Points/src/main/java/com/centaurosolutions/com/beacon/points/BeaconPointsApplication.java
@@ -0,0 +1,12 @@
+package com.centaurosolutions.com.beacon.points;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeaconPointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeaconPointsApplication.class, args);
+	}
+}
