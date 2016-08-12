a7e54fad87b49b1f56e0bd082fa4ff7159ae193a
jarivar <jairo@centaurosolutions.com>
2016-08-11 18:07:50 -0600

    detalles visuales y mostrar precio por productos cuando están relacionadas a una promo

--- BeaconKontackt/app/src/main/java/proyecto/cursoandroid/com/jairo/centaurosolutions/beaconkontackttest3/DetailPromo.java
+++ BeaconKontackt/app/src/main/java/proyecto/cursoandroid/com/jairo/centaurosolutions/beaconkontackttest3/DetailPromo.java
@@ -13,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -21,9 +22,11 @@ import com.squareup.picasso.Picasso;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
 import controllers.ServiceController;
 import model.cache.BeaconCache;
 import proyecto.cursoandroid.com.jairo.centaurosolutions.beaconkontackttest3.Adaptadores.CustomAdapterWish;
@@ -31,10 +34,9 @@ import proyecto.cursoandroid.com.jairo.centaurosolutions.beaconkontackttest3.Ent
 import proyecto.cursoandroid.com.jairo.centaurosolutions.beaconkontackttest3.Entities.Wish;
 import utils.NonStaticUtils;
 
-public class DetailPromo extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener
-{
-    TextView tituloPromo, descripcionPromo, points, pointsAction,descriptionMerchant,nameMerchant;
-    ImageView imagenPromo, openHistoryPoints,share,imageMerchant;
+public class DetailPromo extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+    TextView tituloPromo, descripcionPromo, points, pointsAction, descriptionMerchant, nameMerchant, pointsToGive;
+    ImageView imagenPromo, openHistoryPoints, share, imageMerchant;
     String mpoints, userAcumulatedPoints;
     ArrayList<BeaconCache> myBeaconCacheList;
     Button addImage;
@@ -48,9 +50,9 @@ public class DetailPromo extends AppCompatActivity implements Response.Listener<
     String idUser;
     Promociones promo;
     LinearLayout back;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail__promo);
         nonStaticUtils = new NonStaticUtils();
@@ -83,41 +85,44 @@ public class DetailPromo extends AppCompatActivity implements Response.Listener<
         points = (TextView) findViewById(R.id.puntosPromoDetail);
         descripcionPromo = (TextView) findViewById(R.id.descriptionPromoDetai);
         imagenPromo = (ImageView) findViewById(R.id.imagenPromoDetail);
-        imageMerchant=(ImageView)findViewById(R.id.imageMerchant);
-        descriptionMerchant=(TextView)findViewById(R.id.detailMerchant);
-        nameMerchant=(TextView)findViewById(R.id.merchantName);
-
+        imageMerchant = (ImageView) findViewById(R.id.imageMerchant);
+        descriptionMerchant = (TextView) findViewById(R.id.detailMerchant);
+        nameMerchant = (TextView) findViewById(R.id.merchantName);
+        pointsToGive = (TextView) findViewById(R.id.pointsToGive);
         intent = getIntent();
-        openHistoryPoints=(ImageView) actionBarLayout.findViewById(R.id.openHistoryPoints);
-        share=(ImageView)findViewById(R.id.share);
-       // wish=(Button)findViewById(R.id.wishButton);
+        openHistoryPoints = (ImageView) actionBarLayout.findViewById(R.id.openHistoryPoints);
+        share = (ImageView) findViewById(R.id.share);
+        // wish=(Button)findViewById(R.id.wishButton);
         addImage = (Button) actionBarLayout.findViewById(R.id.buttonAdd);
         addImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),WishListActivity.class);
-                intent.putExtra("idUser",idUser);
+                Intent intent = new Intent(getApplicationContext(), WishListActivity.class);
+                intent.putExtra("idUser", idUser);
                 startActivity(intent);
             }
         });
-        promo= (Promociones)intent.getSerializableExtra("Detail");
-        ServiceController imageRequest =  new ServiceController();
+        promo = (Promociones) intent.getSerializableExtra("Detail");
+        ServiceController imageRequest = new ServiceController();
         points.setText(promo.getPuntos() + " pts");
         tituloPromo.setText(promo.getTitulo());
         descripcionPromo.setText(promo.getDescripcion());
-        idMechantProfile= promo.getMerchantId();
+        idMechantProfile = promo.getMerchantId();
+        idPromo = promo.getId();
+        idProduct = promo.getIdProduct();
+
+        if ((!idMechantProfile.isEmpty()) && (!idPromo.isEmpty()) && (!idProduct.isEmpty())) {
+            searchProductPromo(idPromo, idMechantProfile, idProduct);
+        }
         imageRequest.imageRequest(promo.getUrlImagen(), imagenPromo, 0, 0);
-        final String description= descripcionPromo.getText().toString()+" "+promo.getUrlImagen();
-        final String image=promo.getUrlImagen();
+        final String description = descripcionPromo.getText().toString() + " " + promo.getUrlImagen();
+        final String image = promo.getUrlImagen();
         openHistoryPoints.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mpoints.toString().equals("0"))
-                {
+                if (mpoints.toString().equals("0")) {
                     Toast.makeText(getApplication(), "Aun no ha obtenido puntos", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
+                } else {
                     openHistory();
                 }
             }
@@ -125,15 +130,12 @@ public class DetailPromo extends AppCompatActivity implements Response.Listener<
         pointsAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mpoints.toString().equals("0"))
-                {
+                if (mpoints.toString().equals("0")) {
                     Toast.makeText(getApplication(), "Aun no ha obtenido puntos", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
+                } else {
                     openHistory();
                 }
-        }
+            }
         });
         /*wish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -153,56 +155,50 @@ public class DetailPromo extends AppCompatActivity implements Response.Listener<
         });
 
         dataMerchant();
-        myBeaconCacheList = (ArrayList<BeaconCache>)intent.getSerializableExtra("promoDetail");
+        myBeaconCacheList = (ArrayList<BeaconCache>) intent.getSerializableExtra("promoDetail");
 
-        if(myBeaconCacheList != null)
-        {
+        if (myBeaconCacheList != null) {
             int indextoDelete = -1;
-            for (int i = 0; i < myBeaconCacheList.size(); i++ )
-            {
-                if(myBeaconCacheList.get(i).promoId.equals(promo.getId()))
-                {
-                    indextoDelete=i;
+            for (int i = 0; i < myBeaconCacheList.size(); i++) {
+                if (myBeaconCacheList.get(i).promoId.equals(promo.getId())) {
+                    indextoDelete = i;
                 }
             }
-            if( indextoDelete != -1)
-            {
+            if (indextoDelete != -1) {
                 myBeaconCacheList.remove(indextoDelete);
             }
         }
 
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return super.onSupportNavigateUp();
     }
+
     @Override
-    public void onBackPressed()
-    {
-        if(myBeaconCacheList != null)
-        {
+    public void onBackPressed() {
+        if (myBeaconCacheList != null) {
             Intent redirectIntent = new Intent(getApplicationContext(), PullNotificationsActivity.class);
             redirectIntent.putExtra("promoDetail", myBeaconCacheList);
             startActivity(redirectIntent);
-        }
-        else
-        {
+        } else {
             this.finish();
         }
     }
-    public void openHistory(){
+
+    public void openHistory() {
         Intent intent = new Intent(this.getBaseContext(), HistotyPointsActivity.class);
-        intent.putExtra("idUser",idUser);
+        intent.putExtra("idUser", idUser);
         startActivity(intent);
     }
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings)
-        {
+        if (id == R.id.action_settings) {
             return true;
         }
 
@@ -217,45 +213,38 @@ public class DetailPromo extends AppCompatActivity implements Response.Listener<
     @Override
     public void onResponse(JSONObject response) {
         try {
-            if(response.getString("message").equals("null")){
-                response=response.getJSONObject("productData");
-                response=response.getJSONObject("product");
-                String id,name,url = null;
-                int price;
-                id=response.getString("productId");
-                name=response.getString("productName");
-                price=response.getInt("price");
-                ArrayList<String> images= new ArrayList<String>();
-                JSONArray imagesArray= response.getJSONArray("imageUrlList");
-                if (imagesArray != null && imagesArray.length()>0)  {
+            if (response.getString("message").equals("null")) {
+                response = response.getJSONObject("productData");
+                response = response.getJSONObject("product");
+                String id, name, url = null;
+                double price;
+                price = response.getDouble("price");
+                pointsToGive.setText(String.format(getString(R.string.colonSymbol), Double.toString(price)));
+                ArrayList<String> images = new ArrayList<String>();
+                JSONArray imagesArray = response.getJSONArray("imageUrlList");
+                if (imagesArray != null && imagesArray.length() > 0) {
                     int len = imagesArray.length();
-                    for (int l=0;l<len;l++){
+                    for (int l = 0; l < len; l++) {
                         images.add(imagesArray.get(l).toString());
-                        if(images.size()==1) {
-                           url=imagesArray.get(l).toString();
+                        if (images.size() == 1) {
+                            url = imagesArray.get(l).toString();
                         }
                     }
-                }
-                else
-                {
+                } else {
                     images.add("images");
                 }
-                service(id,name,price,url);
-            }
-            else if(response.getString("message").toString().equals("Perfil de tienda encontrado")) {
-              response=response.getJSONObject("merchantProfile");
+                //service(id,name,price,url);
+            } else if (response.getString("message").toString().equals("Perfil de tienda encontrado")) {
+                response = response.getJSONObject("merchantProfile");
                 descriptionMerchant.setText(response.getString("address"));
                 nameMerchant.setText(response.getString("merchantName"));
                 Picasso.with(this).load(response.getString("image")).into(imageMerchant);
-            }
-            else {
+            } else {
                 if (response.getString("message").toString().equals("User updated")) {
                     Toast.makeText(getApplication(), "Añadido correctamente", Toast.LENGTH_SHORT).show();
-                }
-                 else if (response.getString("message").toString().equals("Product already added to wishlist")) {
+                } else if (response.getString("message").toString().equals("Product already added to wishlist")) {
                     Toast.makeText(getApplication(), "El producto ya existe en la lista de deseos", Toast.LENGTH_SHORT).show();
-                }
-                else if(response.getString("message").toString().equals("Perfil de tienda no encontrado")){
+                } else if (response.getString("message").toString().equals("Perfil de tienda no encontrado")) {
                     Toast.makeText(getApplication(), "Esta promocion no se encuentra asociada a una tienda", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -264,46 +253,49 @@ public class DetailPromo extends AppCompatActivity implements Response.Listener<
             e.printStackTrace();
         }
     }
+
     ServiceController serviceController;
     Response.Listener<JSONObject> response;
     Response.ErrorListener responseError;
 
-    public void service(String productId, String productName,int price, String urlImage){
+    public void service(String productId, String productName, int price, String urlImage) {
         serviceController = new ServiceController();
         responseError = this;
         response = this;
         Map<String, Object> mapParams = new HashMap<>();
-        mapParams.put("userId",idUser);
-        mapParams.put("productId",productId);
+        mapParams.put("userId", idUser);
+        mapParams.put("productId", productId);
         mapParams.put("productName", productName);
-        mapParams.put("price",price);
+        mapParams.put("price", price);
         mapParams.put("imageUrlList", urlImage);
         Map<String, String> map = new HashMap<String, String>();
         map.put("Content-Type", "application/json");
-        String url = getString(R.string.WebService_User)+"user/wishlist/add";
+        String url = getString(R.string.WebService_User) + "user/wishlist/add";
         serviceController.jsonObjectRequest(url, Request.Method.POST, mapParams, map, response, responseError);
     }
-    public void searchProductPromo(){
+
+    public void searchProductPromo(String promoId, String merchantId, String productId) {
         serviceController = new ServiceController();
         responseError = this;
         response = this;
         Map<String, Object> mapParams = new HashMap<>();
-        mapParams.put("merchantId","");
-        mapParams.put("productId", "");
-        mapParams.put("promoId",promo.getId());
+        mapParams.put("merchantId", merchantId);
+        mapParams.put("productId", productId);
+        mapParams.put("promoId", promoId);
         Map<String, String> map = new HashMap<String, String>();
         map.put("Content-Type", "application/json");
-        String url = getString(R.string.WebService_Utils)+"utils/product/getdata";
+        String url = getString(R.string.WebService_Utils) + "utils/product/getdata";
         serviceController.jsonObjectRequest(url, Request.Method.POST, mapParams, map, response, responseError);
     }
-    public void dataMerchant(){
+
+    public void dataMerchant() {
         serviceController = new ServiceController();
         responseError = this;
         response = this;
         Map<String, String> nullMap = new HashMap<String, String>();
         Map<String, String> map = new HashMap<String, String>();
         map.put("Content-Type", "application/json");
-        String url = getString(R.string.WebService_MerchantProfile)+"merchantprofile/"+idMechantProfile;
+        String url = getString(R.string.WebService_MerchantProfile) + "merchantprofile/" + idMechantProfile;
         serviceController.jsonObjectRequest(url, Request.Method.GET, null, map, response, responseError);
     }
 }
